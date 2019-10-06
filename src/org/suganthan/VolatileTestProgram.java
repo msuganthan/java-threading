cf36b391d6ca3579b70be6ad38c3d357fcce6df6
suganthan <suganthan.friartuck.net>
2019-10-06 11:57:01 +0800

    Volatile keyword

---
+++ src/org/suganthan/VolatileTestProgram.java
@@ -0,0 +1,36 @@
+package org.suganthan;
+
+public class VolatileTestProgram {
+
+    private static boolean running = false;
+    //private static volatile boolean running = false;
+
+    public static void main(String[] args) throws Exception{
+
+        //Starting a new thread.
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Wait for running to become true.
+                while (!running) {}
+                System.out.println("Started...");
+
+                //Wait for running to become false.
+                while (running) {}
+                System.out.println("Stopped...");
+            }
+        }).start();
+
+        //Wait for one second
+        Thread.sleep(3000);
+        System.out.println("Starting.");
+        //Set running to true
+        running = true;
+
+        //Wait for one second
+        Thread.sleep(3000);
+        System.out.println("Stopping.");
+        //Set running to false
+        running = false;
+    }
+}

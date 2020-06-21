afcc196415cab3703e5990b8d887e751d1e117c4
suganthan <suganthan.friartuck.net>
2020-06-21 21:34:30 +0800

    Deferred callback

---
+++ src/org/suganthan/deferredCallback/Demonstration.java
@@ -0,0 +1,12 @@
+package org.suganthan.deferredCallback;
+
+/**
+ * Asynchronous programming involves being able to execute functions at a future occurrence of some event. Design a
+ * thread-safe deferred callback class becomes a challenging interview question.
+ *
+ */
+public class Demonstration {
+    public static void main(String[] args) {
+
+    }
+}

6428c98da1f0cba8ea14a80af7bd2b7f92f8ac63
msuganthan <suganthanchandrika@gmail.com>
2021-01-06 17:55:25 +0800

    Token Bucket Filter implementation

--- src/org/suganthan/threadingProblems/tokenBucketFilter/TokenBucketFilter.java
+++ src/org/suganthan/threadingProblems/tokenBucketFilter/TokenBucketFilter.java
@@ -4,28 +4,16 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TokenBucketFilter {
+    long possibleTokens = 0;
     private int MAX_TOKENS;
     private long lastRequestTime = System.currentTimeMillis();
-    long possibleTokens = 0;
 
     public TokenBucketFilter(int maxTokens) {
         MAX_TOKENS = maxTokens;
     }
 
-    synchronized void getToken() throws InterruptedException {
-        //Divide by a 1000 to get granularity at the second level.
-        possibleTokens += (System.currentTimeMillis() - lastRequestTime) / 1000;
-
-        if (possibleTokens > MAX_TOKENS)
-            possibleTokens = MAX_TOKENS;
-
-        if (possibleTokens == 0)
-            Thread.sleep(1000);
-        else
-            possibleTokens--;
-        lastRequestTime = System.currentTimeMillis();
-
-        System.out.println("Granting "+ Thread.currentThread().getName() + " Token at "+ (System.currentTimeMillis() / 1000));
+    public static void main(String[] args) throws InterruptedException {
+        runTextMaxTokenIs1();
     }
 
     static void runTextMaxTokenIs1() throws InterruptedException {
@@ -43,18 +31,33 @@ public class TokenBucketFilter {
                     System.out.println("We have a problem");
                 }
             });
-            thread.setName("Thread_"+(i+1));
+            thread.setName("Thread_" + (i + 1));
             allThreads.add(thread);
         }
 
-        for (Thread t: allThreads)
+        for (Thread t : allThreads)
             t.start();
 
-        for (Thread t: allThreads)
+        for (Thread t : allThreads)
             t.join();
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        runTextMaxTokenIs1();
+    synchronized void getToken() throws InterruptedException {
+        /**
+         * 1. lastRequestTime was initialized to System.currentTimeMillis during the start of the program.
+         * 2. There is Thread.sleep(10000) in runTextMaxTokenIs1 method, the delay makes some token to generate.
+         */
+        possibleTokens += (System.currentTimeMillis() - lastRequestTime) / 1000;
+
+        if (possibleTokens > MAX_TOKENS)
+            possibleTokens = MAX_TOKENS;
+
+        if (possibleTokens == 0)
+            Thread.sleep(1000);
+        else
+            possibleTokens--;
+        lastRequestTime = System.currentTimeMillis();
+
+        System.out.println("Granting " + Thread.currentThread().getName() + " Token at " + (System.currentTimeMillis() / 1000));
     }
 }

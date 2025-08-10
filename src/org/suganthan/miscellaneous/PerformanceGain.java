8a47c585fd9e591aa87d2afc5c8197a8bb355463
Suganth <smadhavan@evertz.com>
2025-08-10 09:54:17 -0400

    Simplify

--- src/org/suganthan/miscellaneous/PerformanceGain.java
+++ src/org/suganthan/miscellaneous/PerformanceGain.java
@@ -1,55 +1,30 @@
 package org.suganthan.miscellaneous;
 
+import static java.lang.Integer.MAX_VALUE;
+
 public class PerformanceGain {
     public static void main(String[] args) throws InterruptedException {
-        SumUpExample.oneThread();
-        SumUpExample.twoThread();
-    }
-}
+        long startTime = System.currentTimeMillis();
+        add(0, MAX_VALUE);
+        System.out.println("Time taken " + (System.currentTimeMillis() - startTime));
 
-class SumUpExample {
-    long start;
-    long end;
-    long counter = 0;
-    static long MAX_NUM = Integer.MAX_VALUE;
-
-    SumUpExample(long start, long end) {
-        this.start = start;
-        this.end   = end;
-    }
-
-    public void add() {
-        for (long i = this.start; i <= this.end; i++) {
-            this.counter += i;
-        }
-    }
-
-    static public void twoThread() throws InterruptedException {
-        long start = System.currentTimeMillis();
-        SumUpExample sumUpExample = new SumUpExample(1, MAX_NUM/2);
-        SumUpExample sumUpExample1 = new SumUpExample(1 + (MAX_NUM/2), MAX_NUM);
-
-        Thread t1 = new Thread(() -> sumUpExample.add());
-
-        Thread t2 = new Thread(() -> sumUpExample1.add());
-
+        long startTime1 = System.currentTimeMillis();
+        int half = MAX_VALUE / 2;
+        Thread t1 = new Thread(() -> add(0, half));
+        Thread t2 = new Thread(() -> add(half, MAX_VALUE));
         t1.start();
-        t1.join();
-
         t2.start();
+
+        t1.join();
         t2.join();
-
-        long finalCount = sumUpExample.counter + sumUpExample1.counter;
-        long end = System.currentTimeMillis();
-        System.out.println("Two threads final count = " + finalCount + " took " + (end - start));
-
+        System.out.println("Time taken " + (System.currentTimeMillis() - startTime1));
     }
-    
-    static public void oneThread() {
-        long start = System.currentTimeMillis();
-        SumUpExample sumUpExample = new SumUpExample(1, MAX_NUM);
-        sumUpExample.add();
-        long end = System.currentTimeMillis();
-        System.out.println("Single thread final count = " + sumUpExample.counter + " took " + (end - start));
+
+    public static long add(int start, int end) {
+        long sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
